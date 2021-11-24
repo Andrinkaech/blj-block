7e39d85c6c7e72170dd76de57acc176857a3fa96
Andrin Käch <Andrin.kaech@gmail1.com>
2021-11-24 14:18:09 +0100

    initial commit

---
+++ HTML/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/navigatorstyle.css">
+
+    <title>Document</title>
+
+
+</head>
+<body>
+
+<div class="grid-container">
+  <div class="grid-item1" id = "titel">Mein Blog</div>
+  <div class= "navi"><?php include 'navigation.php' ?></div>
+  <div class="grid-item2">Blog einträge</div>
+  <div class="grid-item3">Links</div>
+  <div class="grid-item4">footer</div>
+
+
+</div>
+
+
+
+</body>
+</html>
---
+++ HTML/navigation.php
@@ -0,0 +1,9 @@
+
+<nav>
+    <ol class = "navistyle">
+     <li class = "space">Startseite</li>
+      <li class = "space">Alle Blogs</li>
+      <li class = "space noabstand"><a rel=" blog erstellen" href="../HTML/blog_erstellen.php">Blog Erstellen </a></li>
+      <li class = "space">ergendöppis</li>
+    </ol>
+</nav>
